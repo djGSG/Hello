9baea9c758eff76030a45947f8b5dc24936412b6
DJ <DJ>
2017-01-04 17:21:42 +0530

    modified java class

--- HelloJava.java
+++ HelloJava.java
@@ -1,11 +1,6 @@
- Volume in drive C has no label.
- Volume Serial Number is 062F-BCC3
-
- Directory of C:\Users\user\Desktop\JavaTest
-
-01/04/2017  03:13 PM    <DIR>          .
-01/04/2017  03:13 PM    <DIR>          ..
-01/04/2017  01:33 PM               426 HelloJava.class
-01/04/2017  03:15 PM                 0 HelloJava.java
-               2 File(s)            426 bytes
-               2 Dir(s)  750,780,100,608 bytes free
+public class HelloJava{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
